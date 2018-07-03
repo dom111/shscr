13f5ec9791bdc184f9b27d0eb0ad0682d9392921
geremachek <mrender2005@gmail.com>
2018-07-02 19:20:56 -0500

    Create shcolors.c

---
+++ shcolors.c
@@ -0,0 +1,16 @@
+/*
+written by geremachek
+*/
+#include <stdio.h>
+int main()
+{
+	while (1 == 1)
+	{
+		printf("\e[31;41m0\e[0m");
+		printf("\e[32;42m0\e[0m");
+		printf("\e[33;43m0\e[0m");
+		printf("\e[34;44m0\e[0m");
+		printf("\e[35;45m0\e[0m");
+		printf("\e[36;46m0\e[0m");
+	}
+}
