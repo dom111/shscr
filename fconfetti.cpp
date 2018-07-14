405fa1bd3da463ce93ee0b4b218e66658c63f6ef
geremachek <mrender2005@gmail.com>
2018-07-13 20:07:14 -0700

    Create fconfetti.cpp

---
+++ fconfetti.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	system("clear");
+	string index[] = {"`", "~", "!" , "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"};
+	string index_colors[] = {"93", "91", "92", "94", "95"};
+	
+	while(true)
+	{
+		
+		cout << "\e[" << index_colors[rand()%5] << "m" << index[rand()%32] << " ";
+		fflush(stdout);
+		usleep(3);
+		
+	}
+}
