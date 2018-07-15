0029d360a1344d150e2d76f9c435e6ba816ada87
geremachek <mrender2005@gmail.com>
2018-07-15 13:07:36 -0700

    Create bmatrix.cpp

---
+++ bmatrix.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	
+	while(true)
+	{
+		int col = size.ws_col-1;
+		cout << string(rand()%col, ' ');
+		cout << "\e[32m" << rand()%2 << "\e[0m" << "\n";
+		fflush(stdout);
+		usleep(25 * 1000);
+	}
+}
