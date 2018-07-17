315a9020f0b0b8ba86465be786404f0b87025d6e
geremachek <mrender2005@gmail.com>
2018-07-17 13:08:14 -0700

    Update fconfetti.cpp

--- fconfetti.cpp
+++ fconfetti.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 
 using namespace std;
 
@@ -13,13 +14,18 @@ int main()
 	system("clear");
 	string index[] = {"`", "~", "!" , "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"};
 	string index_colors[] = {"93", "91", "92", "94", "95"};
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
 	
 	while(true)
 	{
-		
-		cout << "\e[" << index_colors[rand()%5] << "m" << index[rand()%32] << " ";
+		for(int i = 0; i < size.ws_col-1; i++)
+		{
+			cout << "\e[" << index_colors[rand()%5] << "m" << index[rand()%32] << " ";
+		}
+	
+		cout << "\n";
 		fflush(stdout);
-		usleep(3);
-		
+		usleep(50 * 1000);
 	}
 }
