3d433028647a9eb8ed9f7e51cfd96f77da0a4eba
geremachek <mrender2005@gmail.com>
2018-07-16 19:07:47 -0700

    Create rpix.cpp

---
+++ rpix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	
+	string index_colors[] = {"93", "91", "92", "94", "95"};
+	
+	while(true)
+	{
+		for(int i = 0; i < size.ws_col-1; i++)
+		{
+			cout << "\e[" << index_colors[rand()%5] << "m" << ". ";
+		}
+	
+		cout << "\n";
+		fflush(stdout);
+		usleep(50 * 1000);
+	}
+}
