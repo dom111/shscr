12b794e7f244b6d4fb7ed8b07afbc4686437f882
geremachek <mrender2005@gmail.com>
2018-07-18 17:35:39 -0700

    Create gook.cpp

---
+++ gook.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <unistd.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	system("clear");
+	string index[] = {"q", "w", "e", "r", "t", "y" "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "};
+	srand(time(NULL));
+	
+
+	while(true)
+	{
+		
+		cout << index[rand()%36];
+		fflush(stdout);
+		usleep(50 * 1000);
+		
+	}
+}
