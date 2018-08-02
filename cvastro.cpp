fb697da7040373e13c329125f8ecf6ba897b43ae
geremachek <mrender2005@gmail.com>
2018-08-02 10:04:37 -0700

    Add files via upload

---
+++ cvastro.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main()
+{
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	
+	system("setterm -cursor off");
+	
+	system("clear");
+
+	string index[] = {"37", "90", "97", "39"};
+
+	srand(time(NULL));	
+	
+	int sizex = size.ws_col;
+	
+	int rowx = size.ws_row;
+
+	while(true)
+	{
+		cout << "\033[" << rand()%rowx << ";" << rand()%sizex << "f";
+		cout << "\e[" << index[rand()%4] << "m" << " . " << "\e[0m";		
+		fflush(stdout);
+		usleep(10 * 1000);
+	}
+}
