34795dafab63fedbdd700b37c2f79875fd675d30
geremachek <mrender2005@gmail.com>
2018-08-02 10:04:23 -0700

    Add files via upload

---
+++ airmail.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int check = 1;
+	
+	int check2 = 0;
+
+	string index[] = {"91", "33", "93", "92", "94", "35", "95"};	
+
+	system("clear");	
+	
+	string ui;
+	
+	string a1 = argv[1];
+	
+	if(a1 == "color")
+	{
+		check = 2;
+		check2 = 1;
+	}
+
+	for(int i = check; i < argc; i++)
+	{
+		ui = ui + argv[i] + " ";
+	}
+	struct winsize size;
+	ioctl(STDOUT_FILENO,TIOCGWINSZ,&size);
+	
+	system("setterm -cursor off");
+
+	srand(time(NULL));	
+	
+	int sizex = size.ws_col;
+	
+	int rowx = size.ws_row;
+
+	while(true)
+	{
+		cout << "\033[" << rand()%rowx << ";" << rand()%sizex << "f";
+		if(check2 == 1)
+		{
+			cout << "\e[" << index[rand()%7] << "m" << ui << "\e[0m";
+			fflush(stdout);
+  			usleep(50 * 1000);
+		}
+		else
+		{
+			cout << ui;	
+			fflush(stdout);
+			usleep(50 * 1000);
+		}
+	}
+}
