6df5998ee6b9cba301183a5b8ce3ce2a4eef3617
geremachek <mrender2005@gmail.com>
2018-08-02 21:23:02 -0700

    Update serpent.cpp

--- serpent.cpp
+++ serpent.cpp
@@ -24,13 +24,13 @@ int main(int argc, char *argv[])
 
 	system("setterm -cursor off");
 
-	string index[] = {"\033[1A", "\033[1B", "\033[1C", "\033[1D"};
+	string index[] = {"\033[1A", "\033[1B", "\033[1C", "\033[1D", "\033[1C", "\033[1D"};
 	
 	srand(time(NULL));
 
 	while(true)
 	{
-		cout << index[rand()%4];
+		cout << index[rand()%6];
 		cout << ico;
 		cout << "\033[1D";
 		fflush(stdout);
