e271f256eeadb97bc580f57cb3e70e542994d6ab
geremachek <mrender2005@gmail.com>
2018-08-02 21:22:22 -0700

    Update cdotfill.cpp

--- cdotfill.cpp
+++ cdotfill.cpp
@@ -15,7 +15,7 @@ int main()
 	
 	system("clear");
 
-	string index[] = {"96", "91", "92", "94", "95"};
+	string index[] = {"31", "32", "34", "90","31;1", "32;1", "34;1", "90;1"};
 
 	srand(time(NULL));	
 	
@@ -26,7 +26,7 @@ int main()
 	while(true)
 	{
 		cout << "\033[" << rand()%rowx << ";" << rand()%sizex << "f";
-		cout << "\e[" << index[rand()%5] << "m" << "          "  << " . " << "\e[0m";		
+		cout << "\e[" << index[rand()%8] << "m" << "          "  << " . " << "\e[0m";		
 		fflush(stdout);
 		usleep(10 * 1000);
 	}
