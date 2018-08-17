6f5e6b839bdb0cbc19683b4169811f02b5268e7e
geremachek <mrender2005@gmail.com>
2018-08-17 12:52:56 -0700

    Add files via upload

--- vfstix.cpp
+++ vfstix.cpp
@@ -24,18 +24,16 @@ int main(int argc, char *argv[])
 	
 	int rowx = size.ws_row-10;
 	
-	string index[] = {"91", "33", "93", "92", "94", "35", "95", "96"};
-
 	while(true)
 	{
 		int counter = 0;
 		
 		cout << "\033[" << rand()%rowx << ";" << rand()%sizex << "f";
-		string color = index[rand()%8];
+		string color = to_string(rand()%256);
 		while(counter <= 10)
 		{
 			counter++;	
-			cout << "\e[" << color << ";1" << "m";
+			cout << "\e[38;5;" << color << ";1" << "m";
 			cout << "│";
 			cout << "\e[0m";
 			cout << "\033[1D";
